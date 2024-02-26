1f731a779ca36b95aeb6d3952761f57377255726
Justin Donnelly <justinrdonnelly@gmail.com>
2024-02-26 18:57:15 -0500

    Use InternalChildren instead of properties
    Properties might be more powerful, but I don't think we'll need them.

--- src/window.js
+++ src/window.js
@@ -16,30 +16,11 @@ import Adw from 'gi://Adw';
 export const ZoneDefenseWindow = GObject.registerClass({
     GTypeName: 'ZoneDefenseWindow',
     Template: 'resource:///com/github/justinrdonnelly/ZoneDefense/window.ui',
-    Properties: {
-        'example-property': GObject.ParamSpec.string(
-            'example-property',
-            'Example Property',
-            'A read-write string property',
-            GObject.ParamFlags.READWRITE,
-            null
-        ),
-    }, InternalChildren: ['networkLabel'],
+    InternalChildren: ['networkLabel'],
 }, class ZoneDefenseWindow extends Adw.ApplicationWindow {
-    constructor(application, value) {
+    constructor(application, connectionId) {
         super({ application });
-        this.example_property = `<span size="x-small">You are connected to: ${value}</span>`; // go through the setter to call `notify`
-    }
-    set example_property(value) {
-        if (this.example_property === value) // Is it intentional to go through the getter? https://gjs.guide/guides/gobject/subclassing.html#declaring-properties
-            return;
-        this._example_property = value;
-        this.notify('example-property');
-    }
-    get example_property() {
-        if (this._example_property === undefined)
-            this._example_property = null;
-        return this._example_property;
+        this._networkLabel.label = `<span size="x-small">You are connected to: ${connectionId}</span>`;
     }
 });
 
