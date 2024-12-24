8ac0de3ee421528941f37d38201ea0d47c0e19a3
Justin Donnelly <justinrdonnelly@gmail.com>
2024-12-24 11:21:14 -0500

    Move promisify into main
    Do it once, for everyone to use.

--- src/main.js
+++ src/main.js
@@ -39,6 +39,7 @@ export const ZoneDefenseApplication = GObject.registerClass(
             });
 
             console.log('Welcome to Zone Defense! Starting up.');
+            this.#promisify();
             this.#connectionIdsSeen = new ConnectionIdsSeen();
 
             // quit action
@@ -195,6 +196,33 @@ export const ZoneDefenseApplication = GObject.registerClass(
             this._showAboutAction.disconnect(this._showAboutActionHandlerId);
             this._connectionChangedAction.disconnect(this._connectionChangedActionHandlerId);
             super.quit(); // this ends up calling vfunc_shutdown()
+        }
+
+        #promisify() {
+            // https://gjs.guide/guides/gio/file-operations.html#file-operations
+            // We'll comment out the things we don't need
+            // Gio._promisify(Gio.File.prototype, 'copy_async');
+            // Gio._promisify(Gio.File.prototype, 'create_async');
+            // Gio._promisify(Gio.File.prototype, 'delete_async');
+            // Gio._promisify(Gio.File.prototype, 'enumerate_children_async');
+            Gio._promisify(Gio.File.prototype, 'load_contents_async');
+            // Gio._promisify(Gio.File.prototype, 'make_directory_async');
+            // Gio._promisify(Gio.File.prototype, 'move_async');
+            // Gio._promisify(Gio.File.prototype, 'open_readwrite_async');
+            // Gio._promisify(Gio.File.prototype, 'query_info_async');
+            Gio._promisify(Gio.File.prototype, 'replace_contents_async');
+            // Gio._promisify(Gio.File.prototype, 'replace_contents_bytes_async',
+            //     'replace_contents_finish');
+            // Gio._promisify(Gio.File.prototype, 'trash_async');
+
+            /* Gio.FileEnumerator */
+            Gio._promisify(Gio.FileEnumerator.prototype, 'next_files_async');
+
+            /* Gio.InputStream */
+            // Gio._promisify(Gio.InputStream.prototype, 'read_bytes_async');
+
+            /* Gio.OutputStream */
+            // Gio._promisify(Gio.OutputStream.prototype, 'write_bytes_async');
         }
     }
 );
