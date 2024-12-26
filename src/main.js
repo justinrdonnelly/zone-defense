821dd73142803437c5406bcf2f984fadb7852ba8
Justin Donnelly <justinrdonnelly@gmail.com>
2024-12-26 17:31:37 -0500

    Don't include a quit action
    This was generated by GNOME Builder from the template, but doesn't seem
    to be used. We can add it back if necessary.

--- src/main.js
+++ src/main.js
@@ -44,14 +44,6 @@ export const ZoneDefenseApplication = GObject.registerClass(
             this.#promisify();
             this.#connectionIdsSeen = new ConnectionIdsSeen();
 
-            // quit action
-            this._quitAction = new Gio.SimpleAction({ name: 'quit' });
-            // eslint-disable-next-line no-unused-vars
-            this._quitActionHandlerId = this._quitAction.connect('activate', (action) => {
-                this.quit(null);
-            });
-            this.add_action(this._quitAction);
-
             // about action
             this._showAboutAction = new Gio.SimpleAction({ name: 'about' });
             // eslint-disable-next-line no-unused-vars
@@ -203,7 +195,6 @@ export const ZoneDefenseApplication = GObject.registerClass(
             this.#sourceIds.forEach((id) => GLib.Source.remove(id));
             this.networkState?.destroy();
             this.networkState = null;
-            this._quitAction.disconnect(this._quitActionHandlerId);
             this._showAboutAction.disconnect(this._showAboutActionHandlerId);
             this._connectionChangedAction.disconnect(this._connectionChangedActionHandlerId);
             super.quit(); // this ends up calling vfunc_shutdown()
